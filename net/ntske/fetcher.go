799c988a4a6d5c1fddfe46d28c767999531f6cee
Aaron Bojarski <eques.superbus@gmail.com>
2023-03-24 12:09:31 +0100

    inline exchangeKeys

--- net/ntske/fetcher.go
+++ net/ntske/fetcher.go
@@ -14,10 +14,28 @@ type Fetcher struct {
 }
 
 func (f *Fetcher) exchangeKeys() error {
-	ke, err := exchangeKeys(&f.TLSConfig, false)
+	ke, err := Connect(f.TLSConfig.ServerName, &f.TLSConfig, false)
 	if err != nil {
 		return err
 	}
+
+	err = ke.Exchange()
+	if err != nil {
+		return err
+	}
+
+	if len(ke.Meta.Cookie) == 0 {
+		return errors.New("unexpected NTS-KE meta data: no cookies")
+	}
+	if ke.Meta.Algo != AES_SIV_CMAC_256 {
+		return errors.New("unexpected NTS-KE meta data: unknown algorithm")
+	}
+
+	err = ke.ExportKeys()
+	if err != nil {
+		return err
+	}
+
 	logNTSKEMetadata(f.Log, ke.Meta)
 	f.data = ke.Meta
 	return nil
@@ -44,31 +62,4 @@ func isValid(data Data) bool {
 
 func (f *Fetcher) StoreCookie(cookie []byte) {
 	f.data.Cookie = append(f.data.Cookie, cookie)
-}
-
-func exchangeKeys(c *tls.Config, debug bool) (*KeyExchange, error) {
-	ke, err := Connect(c.ServerName, c, debug)
-	if err != nil {
-		return nil, err
-	}
-
-	err = ke.Exchange()
-	if err != nil {
-		return nil, err
-	}
-
-	if len(ke.Meta.Cookie) == 0 {
-		return nil, errors.New("unexpected NTS-KE meta data: no cookies")
-	}
-
-	if ke.Meta.Algo != AES_SIV_CMAC_256 {
-		return nil, errors.New("unexpected NTS-KE meta data: unknown algorithm")
-	}
-
-	err = ke.ExportKeys()
-	if err != nil {
-		return nil, err
-	}
-
-	return ke, nil
 }
