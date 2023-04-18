af23fce7880465efa1b637cb235aa321fc1b4a3b
Aaron Bojarski <eques.superbus@gmail.com>
2023-04-18 13:26:42 +0200

    factor out handleKeyExchange

--- core/server/ntske.go
+++ core/server/ntske.go
@@ -13,71 +13,76 @@ import (
 
 const defaultNtskePort int = 4460
 
+func handleKeyExchange(log *zap.Logger, ke *ntske.KeyExchange, localHost *net.UDPAddr, provider *ntske.Provider) {
+	defer ke.Conn.Close()
+
+	err := ke.Read()
+	if err != nil {
+		log.Info("failed to read key exchange", zap.Error(err))
+		return
+	}
+
+	err = ke.ExportKeys()
+	if err != nil {
+		log.Info("failed to export keys", zap.Error(err))
+		return
+	}
+
+	var msg ntske.ExchangeMsg
+	msg.AddRecord(ntske.NextProto{
+		NextProto: ntske.NTPv4,
+	})
+	msg.AddRecord(ntske.Algorithm{
+		Algo: []uint16{ntske.AES_SIV_CMAC_256},
+	})
+	msg.AddRecord(ntske.Server{
+		Addr: []byte(localHost.IP.String()),
+	})
+	msg.AddRecord(ntske.Port{
+		Port: uint16(localHost.Port),
+	})
+
+	var plaintextCookie ntske.ServerCookie
+	plaintextCookie.Algo = ntske.AES_SIV_CMAC_256
+	plaintextCookie.C2S = ke.Meta.C2sKey
+	plaintextCookie.S2C = ke.Meta.S2cKey
+	key := provider.Current()
+	for i := 0; i < 8; i++ {
+		encryptedCookie, err := plaintextCookie.EncryptWithNonce(key.Value, key.Id)
+		if err != nil {
+			log.Info("failed to encrypt cookie", zap.Error(err))
+			continue
+		}
+
+		b := encryptedCookie.Encode()
+		msg.AddRecord(ntske.Cookie{
+			Cookie: b,
+		})
+	}
+
+	msg.AddRecord(ntske.End{})
+
+	buf, err := msg.Pack()
+	if err != nil {
+		log.Info("failed to build packet", zap.Error(err))
+		return
+	}
+
+	n, err := ke.Conn.Write(buf.Bytes())
+	if err != nil || n != buf.Len() {
+		log.Info("failed to write response", zap.Error(err))
+		return
+	}
+}
+
 func runNTSKEServer(log *zap.Logger, listener net.Listener, localHost *net.UDPAddr, provider *ntske.Provider) {
 	for {
 		ke, err := ntske.NewListener(listener)
 		if err != nil {
 			log.Info("failed to accept client", zap.Error(err))
-			break
-		}
-
-		err = ke.Read()
-		if err != nil {
-			log.Info("failed to read key exchange", zap.Error(err))
-			return
-		}
-
-		err = ke.ExportKeys()
-		if err != nil {
-			log.Info("failed to export keys", zap.Error(err))
-			return
-		}
-
-		var msg ntske.ExchangeMsg
-		msg.AddRecord(ntske.NextProto{
-			NextProto: ntske.NTPv4,
-		})
-		msg.AddRecord(ntske.Algorithm{
-			Algo: []uint16{ntske.AES_SIV_CMAC_256},
-		})
-		msg.AddRecord(ntske.Server{
-			Addr: []byte(localHost.IP.String()),
-		})
-		msg.AddRecord(ntske.Port{
-			Port: uint16(localHost.Port),
-		})
-
-		var plaintextCookie ntske.ServerCookie
-		plaintextCookie.Algo = ntske.AES_SIV_CMAC_256
-		plaintextCookie.C2S = ke.Meta.C2sKey
-		plaintextCookie.S2C = ke.Meta.S2cKey
-		key := provider.Current()
-		for i := 0; i < 8; i++ {
-			encryptedCookie, err := plaintextCookie.EncryptWithNonce(key.Value, key.Id)
-			if err != nil {
-				log.Info("failed to encrypt cookie", zap.Error(err))
-				continue
-			}
-
-			b := encryptedCookie.Encode()
-			msg.AddRecord(ntske.Cookie{
-				Cookie: b,
-			})
-		}
-
-		msg.AddRecord(ntske.End{})
-
-		buf, err := msg.Pack()
-		if err != nil {
-			log.Info("failed to build packet", zap.Error(err))
 			continue
 		}
-
-		n, err := ke.Conn.Write(buf.Bytes())
-		if err != nil || n != buf.Len() {
-			log.Info("failed to write response", zap.Error(err))
-			continue
-		}
+		handleKeyExchange(log, ke, localHost, provider)
 	}
 }
 
