5002d921b1f26b249f9135c3c71bf21f0a27b660
Marc Frei <marc.frei@gmail.com>
2025-11-16 15:03:33 +0100

    Exetend CSPTP test driver to support SCION

--- timeservice_t.go
+++ timeservice_t.go
@@ -7,16 +7,19 @@ import (
 	"flag"
 	"fmt"
 	"log/slog"
-	"net"
 	"net/netip"
 	"os"
 	"time"
+
+	"github.com/scionproto/scion/pkg/snet"
+	"github.com/scionproto/scion/pkg/snet/path"
 
 	"example.com/scion-time/base/logbase"
 	"example.com/scion-time/core/client"
 	"example.com/scion-time/core/server"
 	"example.com/scion-time/core/timebase"
 	"example.com/scion-time/driver/clocks"
+	"example.com/scion-time/net/udp"
 )
 
 func runT() {
@@ -46,31 +49,87 @@ func runT() {
 	timebase.RegisterClock(lclk)
 
 	if raddr == "" {
-		localAddr := netip.MustParseAddr(laddr)
-		localHost := net.UDPAddrFromAddrPort(netip.AddrPortFrom(localAddr, 0))
-
-		server.StartCSPTPServerIP(ctx, log, localHost, uint8(dscp))
-
+		// Server mode
+		var localAddr snet.UDPAddr
+		if err := localAddr.Set(laddr); err != nil {
+			panic("failed to parse local address")
+		}
+		if localAddr.IA.IsZero() {
+			if ip4 := localAddr.Host.IP.To4(); ip4 != nil {
+				localAddr.Host.IP = ip4
+			}
+			server.StartCSPTPServerIP(ctx, log, localAddr.Host, uint8(dscp))
+		} else {
+			server.StartCSPTPServerSCION(ctx, log, localAddr.Host, uint8(dscp))
+		}
 		select {}
 	} else {
-		localAddr := netip.MustParseAddr(laddr)
-		remoteAddr := netip.MustParseAddr(raddr)
-
-		c := &client.CSPTPClientIP{
-			Log:  log,
-			DSCP: uint8(dscp),
+		// Client mode
+		var localAddr snet.UDPAddr
+		if err := localAddr.Set(laddr); err != nil {
+			panic("failed to parse local address")
 		}
-
-		for {
-			ts, off, err := c.MeasureClockOffset(ctx, localAddr, remoteAddr)
-			if err != nil {
-				logbase.Fatal(slog.Default(), "failed to measure clock offset", slog.Any("remote", raddr), slog.Any("error", err))
+		var remoteAddr snet.UDPAddr
+		if err = remoteAddr.Set(raddr); err != nil {
+			panic("failed to parse remote address")
+		}
+		if remoteAddr.IA.IsZero() {
+			if ip4 := localAddr.Host.IP.To4(); ip4 != nil {
+				localAddr.Host.IP = ip4
 			}
-			if !periodic {
-				break
+			laddr, ok := netip.AddrFromSlice(localAddr.Host.IP)
+			if !ok {
+				panic("unexpected address type")
 			}
-			fmt.Printf("%s,%+.9f\n", ts.UTC().Format(time.RFC3339), off.Seconds())
-			lclk.Sleep(1 * time.Second)
+			if ip4 := remoteAddr.Host.IP.To4(); ip4 != nil {
+				remoteAddr.Host.IP = ip4
+			}
+			raddr, ok := netip.AddrFromSlice(remoteAddr.Host.IP)
+			if !ok {
+				panic("unexpected address type")
+			}
+			c := &client.CSPTPClientIP{
+				Log:  log,
+				DSCP: uint8(dscp),
+			}
+			for {
+				ts, off, err := c.MeasureClockOffset(ctx, laddr, raddr)
+				if err != nil {
+					logbase.Fatal(slog.Default(), "failed to measure clock offset", slog.Any("remote", raddr), slog.Any("error", err))
+				}
+				if !periodic {
+					break
+				}
+				fmt.Printf("%s,%+.9f\n", ts.UTC().Format(time.RFC3339), off.Seconds())
+				lclk.Sleep(1 * time.Second)
+			}
+		} else {
+			if remoteAddr.IA != localAddr.IA {
+				panic("not yet implemented")
+			}
+			laddr := udp.UDPAddrFromSnet(&localAddr)
+			raddr := udp.UDPAddrFromSnet(&remoteAddr)
+			p := path.Path{
+				Src:           localAddr.IA,
+				Dst:           remoteAddr.IA,
+				DataplanePath: path.Empty{},
+				NextHop:       remoteAddr.Host,
+			}
+			c := &client.CSPTPClientSCION{
+				Log:  log,
+				DSCP: uint8(dscp),
+			}
+			for {
+				ts, off, err := c.MeasureClockOffset(ctx, laddr, raddr, p)
+				if err != nil {
+					logbase.Fatal(slog.Default(), "failed to measure clock offset", slog.Any("remote", raddr), slog.Any("error", err))
+				}
+				if !periodic {
+					break
+				}
+				fmt.Printf("%s,%+.9f\n", ts.UTC().Format(time.RFC3339), off.Seconds())
+				lclk.Sleep(1 * time.Second)
+			}
 		}
 	}
 }
