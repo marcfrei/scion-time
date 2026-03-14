8de6469c84ccd90b7e8153c332041269b63db408
Marc Frei <marc.frei@gmail.com>
2026-03-14 11:12:36 +0100

    Fix checks on darwin

--- core/sync/adjustments/pi_linux.go
+++ core/sync/adjustments/pi_linux.go
@@ -49,6 +49,29 @@ import (
 	"example.com/scion-time/base/unixutil"
 )
 
+type PIController struct {
+	// Clock defaults to CLOCK_REALTIME when empty. On Linux, a device path such
+	// as /dev/ptp0 selects a PTP hardware clock.
+	Clock string
+
+	// Ratio (gain factor) of the proportional control output value (applied to
+	// the measured offset).
+	KP float64
+
+	// Ratio of the integral control output value. The integral value is applied
+	// by reverting only a part of the previous adjustment. This ratio defines the
+	// part of the previous adjustment that is to be kept. That means, that the
+	// size of the integral control output depends on both of the configurable
+	// ratios of the PI controller.
+	KI float64
+
+	// Offset threshold indicating that, if reached, a clock step is to be applied.
+	StepThreshold time.Duration
+
+	clockID, clockFD       int
+	p, i, freq, freqAddend float64
+}
+
 var _ Adjustment = (*PIController)(nil)
 
 var (
--- core/sync/adjustments/pi_std.go
+++ core/sync/adjustments/pi_std.go
@@ -8,6 +8,26 @@ import (
 	"time"
 )
 
+type PIController struct {
+	// Clock defaults to CLOCK_REALTIME when empty. On Linux, a device path such
+	// as /dev/ptp0 selects a PTP hardware clock.
+	Clock string
+
+	// Ratio (gain factor) of the proportional control output value (applied to
+	// the measured offset).
+	KP float64
+
+	// Ratio of the integral control output value. The integral value is applied
+	// by reverting only a part of the previous adjustment. This ratio defines the
+	// part of the previous adjustment that is to be kept. That means, that the
+	// size of the integral control output depends on both of the configurable
+	// ratios of the PI controller.
+	KI float64
+
+	// Offset threshold indicating that, if reached, a clock step is to be applied.
+	StepThreshold time.Duration
+}
+
 var _ Adjustment = (*PIController)(nil)
 
 func (a *PIController) Do(offset time.Duration) {
